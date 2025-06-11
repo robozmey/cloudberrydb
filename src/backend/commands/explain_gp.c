25eab6afd52b71f7b342b5872389501c8237347f
Vladimir Rachkin <vova@kpnn.ru>
2025-06-11 11:38:13 +0300

    Remove comma

--- src/backend/commands/explain_gp.c
+++ src/backend/commands/explain_gp.c
@@ -1636,7 +1636,7 @@ cdbexplain_NodeSummary(ExplainState *es, CdbExplain_NodeSummary *ns) {
 		appendStringInfoSpaces(es->str, ns_spaces);
 		appendStringInfoString(es->str, "StatInsts:\n");
 		appendStringInfoSpaces(es->str, ns_spaces + 2);
-		appendStringInfoString(es->str, "(segN) pstype starttime counter firsttuple startup total ntuples ntuples2 nloops nfiltered1 nfiltered2 execmemused workmemused workmemwanted workfileCreated firststart numPartScanned bnotes enotes, nworkers_launched\n");
+		appendStringInfoString(es->str, "(segN) pstype starttime counter firsttuple startup total ntuples ntuples2 nloops nfiltered1 nfiltered2 execmemused workmemused workmemwanted workfileCreated firststart numPartScanned bnotes enotes nworkers_launched\n");
 	}
 	else {
 		ExplainOpenGroup("CdbExplain_NodeSummary", "CdbExplain_NodeSummary", true, es);
